2205466ed1a57129eac097c1bd89d059d89fc662
Abdullah Ahmed <0.abdull.ah.med@gmail.com>
2023-11-07 08:54:54 +0500

    Created MiniExcel

--- MiniExcel/MiniExcel.cpp
+++ MiniExcel/MiniExcel.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-class LinkedList
+/*class LinkedList
 {
 		class Node
 		{
@@ -36,6 +36,81 @@ class LinkedList
 						left = nullptr;
 						up = nullptr;
 						down = nullptr;
+				}
+				Node(Node* up, Node* down)
+				{
+						this->data = "";
+						right = nullptr;
+						left = nullptr;
+						this->up = up;
+						this->down = down;
+				}
+				Node(Node* up, Node* down, Node* right, Node* left)
+				{
+						this->data = "";
+						this->right = right;
+						this->left = left;
+						this->up = up;
+						this->down = down;
+				}
+		};
+};*/
+class MiniExcel
+{
+		class Cell
+		{
+				string data;
+				Cell* right;
+				Cell* left;
+				Cell* up;
+				Cell* down;
+				Cell()
+				{
+						data = "";
+						right = nullptr;
+						left = nullptr;
+						up = nullptr;
+						down = nullptr;
+				}
+				Cell(Cell* right)
+				{
+						this->data = "";
+						this->right = right;
+						left = nullptr;
+						up = nullptr;
+						down = nullptr;
+				}
+				Cell(Cell* up)
+				{
+						this->data = "";
+						right = nullptr;
+						left = nullptr;
+						this->up = up;
+						down = nullptr;
+				}
+				Cell(Cell* up, Cell* down)
+				{
+						this->data = "";
+						right = nullptr;
+						left = nullptr;
+						this->up = up;
+						this->down = down;
+				}
+				Cell(Cell* up, Cell* down, Cell* right, Cell* left)
+				{
+						this->data = "";
+						this->right = right;
+						this->left = left;
+						this->up = up;
+						this->down = down;
+				}
+				Cell(string data, Cell* up, Cell* down, Cell* right, Cell* left)
+				{
+						this->data = data;
+						this->right = right;
+						this->left = left;
+						this->up = up;
+						this->down = down;
 				}
 		};
 };
